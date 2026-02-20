cf255e426363bff3e9c1b03e7928f9a468c0a72e
Marting Lenoir Diego <dmarting@hotmail.be>
2026-02-20 13:24:20 +0100

    init: project stucture

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Space Dashboard", page_icon=":rocket:", layout="wide")
+st.title("🚀 Space Dashboard")
+st.markdown("Bienvenue sur ton dashboard spatial personnel !")
